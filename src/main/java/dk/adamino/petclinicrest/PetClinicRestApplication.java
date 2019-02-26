f73a126ffc1c90cabd95769eb6b45e4f633a4642
Adam Hansen <adamlars90@gmail.com>
2019-02-26 21:30:36 +0100

    Initial commit

---
+++ src/main/java/dk/adamino/petclinicrest/PetClinicRestApplication.java
@@ -0,0 +1,13 @@
+package dk.adamino.petclinicrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetClinicRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetClinicRestApplication.class, args);
+	}
+
+}
